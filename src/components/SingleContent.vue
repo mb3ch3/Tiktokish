aac7b5991743d9f9a09cb9ee94ba6b1296878d4a
mb3ch3 <rhowdan@gmail.com>
2023-01-17 16:42:39 +0300

    create single content for tiktok video

---
+++ src/components/SingleContent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="content">
+    <q-avatar size="55px" class="avatar">
+        <img src="https://cdn.quasar.dev/img/avatar.png"/>
+    </q-avatar>
+    <div class="middle">
+        <div class="heading">
+            <span class="username">diamondplatinum___z</span>
+            <span class="acccname">platinumuztz234</span>
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel non corrupti impedit delectus ipsa eos dolor amet adipisci praesentium quisquam architecto nemo voluptatibus ab accusantium dicta, dolorum laborum maiores in?
+        </div>
+        <div>
+            <!-- <iframe src="https://giphy.com/embed/IRFQYGCokErS0" width="360" height="500" frameBorder="0" class="giphy-embed video" allowFullScreen></iframe> -->
+            <!-- <q-video src="https://www.pexels.com/video/repetitive-footage-of-a-woman-holding-a-mirror-ball-3444227/" /> -->
+            <!-- <q-video :ratio="16/17" src="https://giphy.com/embed/IRFQYGCokErS0"></q-video> -->
+            <video width="280" height="460" controls>
+                <source src="../assets/video.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+    <div class="follow">
+        <q-btn size="11px"  outline style="color: #FE2C55;" label="Follow" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.content{
+    border: 2px solid ;
+    height: 90vh;
+    display:grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    padding: 10px;
+}
+
+.avatar{
+    justify-self: flex-end;
+    margin-right: 15px;
+    margin-top: 10px;
+
+}
+.follow{
+    justify-self: center;
+    margin-top: 10px;
+
+}
+.middle{
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: .1fr auto 3fr;
+    justify-content: flex-start;
+}
+.middle div{
+    border: 1px hotpink solid;
+}
+
+.heading{
+    display: flex;
+    gap: 5px;
+    align-items: center;
+}
+.username{
+    font-weight: bolder;
+    font-size: 18px;
+}
+.video{
+    border: 2px solid;
+}
+
+</style>
